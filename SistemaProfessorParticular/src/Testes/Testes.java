c98e512b89c9b78060c8962a709ea62b5388a976
renatapigatto <renatapigatto@gmail.com>
2023-05-22 21:53:23 -0300

    fix LocalDateTime logic

---
+++ SistemaProfessorParticular/src/Testes/Testes.java
@@ -0,0 +1,14 @@
+package Testes;
+
+public class Testes {
+
+	public static void main(String[] args) {
+		
+		//Aluno a1 = new Aluno("Renata", "999999999", 1);
+		
+		//a1.setNome("Renata");
+		//a1.setCpf("99999999999");
+				
+	}
+
+}
